2688bdcb6bc5c1b5421ceba8bb420c23ea69a7c8
vurtual <VurtualRuler98@gmail.com>
2017-11-14 22:59:07 -0600

    Added 20ft iso, US pallet size, ACE framework support

--- addons/boxloader/config.cpp
+++ addons/boxloader/config.cpp
@@ -1,9 +1,10 @@
 class CfgPatches {
 	class boxloader {
-		units[] = {"Land_Boxloader_Pallet_1","Land_Boxloader_Pallet_1_ed","Land_Boxloader_Pallet_2","Land_Boxloader_Pallet_2_ed"};
+		units[] = {"Land_Boxloader_Pallet_1","Land_Boxloader_Pallet_1_ed","Land_Boxloader_Pallet_2","Land_Boxloader_Pallet_2_ed","Land_Boxloader_Pallet_3","Land_Boxloader_Pallet_3_ed"};
 		weapons[] = {};
 		requiredVersion = 0.1;
 		requiredAddons[] = {};
+		preloadAddons[] = {"ace_interact_menu"};
 	};
 };
 class CfgFunctions {
@@ -16,12 +17,14 @@ class CfgFunctions {
 		class addaction {};
 		class pallet_create {};
 		class pallet_init {};
+		class pallet_addmem {};
 		};
 	};
 };
 class CfgVehicles {
 	class StaticWeapon;
 	class Thing;
+	class Land_Cargo20_military_green_F;
 	class Boxloader_Pallet_base_ed: Thing {
 		editorCategory="EdCat_Supplies";
 		editorSubcategory="EdSubCat_Boxloader";
@@ -72,9 +75,7 @@ class CfgVehicles {
 		};
 	};
 	class Land_Boxloader_Pallet_1_ed: Boxloader_Pallet_base_ed {
-		editorCategory="EdCat_Supplies";
-		editorSubcategory="EdSubCat_Boxloader";
-		displayName = "Boxloader Pallet 1";
+		displayName = "Boxloader Pallet Square";
 		model = "\boxloader\mdl\boxloader_pallet1.p3d";
 		scope=2;
 		scopeCurator=2;
@@ -83,8 +84,6 @@ class CfgVehicles {
 		};
 	};
 	class Land_Boxloader_Pallet_2_ed: Boxloader_Pallet_base_ed {
-		editorCategory="EdCat_Supplies";
-		editorSubcategory="EdSubCat_Boxloader";
 		displayName = "Boxloader EUR-1 Pallet";
 		model = "\boxloader\mdl\boxloader_pallet2.p3d";
 		scope=2;
@@ -93,13 +92,49 @@ class CfgVehicles {
 		init = "[_this select 0,'Land_Boxloader_Pallet_2'] call boxloader_fnc_pallet_create";	
 		};
 	};
+	class Land_Boxloader_Pallet_3_ed: Boxloader_Pallet_base_ed {
+		displayName = "Boxloader Pallet US";
+		model = "\boxloader\mdl\boxloader_pallet3.p3d";
+		scope=2;
+		scopeCurator=2;
+		class EventHandlers {
+		init = "[_this select 0,'Land_Boxloader_Pallet_3'] call boxloader_fnc_pallet_create";	
+		};
+	};
+	class Land_Boxloader_iso20_ed: Land_Cargo20_military_green_F {
+		editorCategory="EdCat_Supplies";
+		editorSubcategory="EdSubCat_Boxloader";
+		displayName = "Boxloader 20ft ISO";
+		scope=2;
+		scopeCurator=2;
+		class EventHandlers {
+		init = "[_this select 0,'Land_Boxloader_iso20',90] call boxloader_fnc_pallet_addmem";	
+		};
+	};
 	class Land_Boxloader_Pallet_1: Boxloader_Pallet_Base {
 		model = "\boxloader\mdl\boxloader_pallet1.p3d";
-		displayName = "Boxloader Pallet 1";
+		displayName = "Boxloader Pallet Square";
 	};
+
 	class Land_Boxloader_Pallet_2: Boxloader_Pallet_Base {
 		model = "\boxloader\mdl\boxloader_pallet2.p3d";
 		displayName = "Boxloader EUR-1 Pallet";
+	};
+	class Land_Boxloader_Pallet_3: Boxloader_Pallet_Base {
+		model = "\boxloader\mdl\boxloader_pallet3.p3d";
+		displayName = "Boxloader Pallet US";
+	};
+	class Land_Boxloader_iso20: Boxloader_Pallet_Base {
+		model = "\boxloader\mdl\boxloader_mem_iso20.p3d";
+		class VehicleTransport: VehicleTransport {
+			class Cargo {
+				canBeTransported            = 0;
+			};
+			class Carrier: Carrier {
+				cargoBayDimensions[]        = {"BBox_Base1", "BBox_Corner"};
+				cargoSpacing[]              = {0.05, 0.05, 0};
+			};
+		};
 	};
 };
 class CfgEditorSubcategories {
---
+++ addons/boxloader/functions/fn_pallet_addmem.sqf
@@ -0,0 +1,17 @@
+params [["_veh",objNull,[objNull]],["_obj","",[""]],["_rot",0,[0]]];
+if (is3DEN) exitWith {false};
+if (hasInterface) then {
+	if (isNull _veh) exitWith {false};
+	if (isClass(configFile >> "CfgPatches" >> "boxloader_ace")) exitWith {_pallet = ((_this select 0) getVariable["boxloader_mem",objNull]); if (!isNull _pallet) then {_pallet setVariable ["boxloader_dontselect",true,true];};true};
+	_veh addAction ["Select container loading target",{(_this select 1) setVariable["boxloader_tgt",((_this select 0) getVariable["boxloader_mem",objNull]),true];},[],0,false,true,"","(vehicle _this == _this) && (isNull (_this getVariable 'boxloader_tgt'))"];
+};
+if (!isServer) exitWith {false};
+if (isNull _veh) exitWith {["You tried to init a pallet on a thing that doesn't exist!"] call bis_fnc_error; false};
+if (!(_obj isKindOf "Boxloader_Pallet_base")) exitWith {["We can't make something into not a pallet."] call bis_fnc_error;false};
+_pallet = _obj createVehicle (position _veh);
+if (isNull _pallet) exitWith {false};
+_pallet attachTo [_veh,[0,0,0]];
+_pallet setDir _rot;
+_veh setVariable ["boxloader_mem",_pallet,true];
+true
+
