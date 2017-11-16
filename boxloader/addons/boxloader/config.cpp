6b28496c4a3415df32b43ecde095206647090ef9
vurtual <VurtualRuler98@gmail.com>
2017-11-16 01:26:08 -0600

    463L pallet and US cargobox pallet object

--- boxloader/addons/boxloader/config.cpp
+++ boxloader/addons/boxloader/config.cpp
@@ -5,7 +5,9 @@ class CfgPatches {
 			"Land_Boxloader_Pallet_2","Land_Boxloader_Pallet_2_ed",
 			"Land_Boxloader_Pallet_3","Land_Boxloader_Pallet_3_ed",
 			"Land_Boxloader_iso20","Land_Boxloader_iso20_ed",
-			"Land_Boxloader_Pallet_4","Land_Boxloader_Pallet_4_ed"
+			"Land_Boxloader_Pallet_4","Land_Boxloader_Pallet_4_ed",
+			"Land_Boxloader_Pallet_5","Land_Boxloader_Pallet_5_ed",
+			"Land_Boxloader_Crate_1"
 		};
 		weapons[] = {};
 		requiredVersion = 0.1;
@@ -30,6 +32,7 @@ class CfgFunctions {
 class CfgVehicles {
 	class StaticWeapon;
 	class Thing;
+	class Reammobox_F;
 	class Land_Cargo20_military_green_F;
 	class Boxloader_Pallet_base_ed: Thing {
 		editorCategory="EdCat_Supplies";
@@ -40,6 +43,33 @@ class CfgVehicles {
 		class EventHandlers {
 		init = "_this call boxloader_fnc_pallet_create";	
 		};
+	};
+	class Land_Boxloader_Crate_1: Reammobox_F {
+		ace_dragging_dragPosition[] = {0,1.5,0};
+		class VehicleTransport {
+			class Cargo {
+				parachuteClass              = B_Parachute_02_F;
+				parachuteHeightLimit        = 40;
+				canBeTransported            = 1;
+				dimensions[]                = {"BBox_Base0", "BBox_Corner_1m"};
+			};
+		   class Carrier {
+			};
+		};
+		slingLoadCargoMemoryPoints[] = { "SlingLoadCargo1","SlingLoadCargo2","SlingLoadCargo3","SlingLoadCargo4"};
+		editorCategory="EdCat_Supplies";
+		editorSubcategory="EdSubCat_Boxloader";
+		displayName = "Boxloader Cargo Box (US Pallet)";
+		model = "\boxloader\mdl\boxloader_crate1.p3d";
+		icon = "iconObject_1x1";
+		scope=2;
+		scopeCurator=2;
+		side=3;
+		maximumLoad=5000;
+		ace_cargo_size = 5;
+		ace_cargo_canLoad = 1;
+		ace_cargo_space = 4;
+		ace_Cargo_hasCargo = 1;
 	};
 	class Boxloader_Pallet_base: StaticWeapon {
 		icon = "iconObject_1x1";
@@ -132,6 +162,13 @@ class CfgVehicles {
 		scopeCurator=1;
 		BoxloaderClass = "Land_Boxloader_Pallet_4";
 	};
+	class Land_Boxloader_Pallet_5_ed: Boxloader_Pallet_base_ed {
+		displayName = "Boxloader 463L Master Pallet";
+		model = "\boxloader\mdl\boxloader_pallet5.p3d";
+		scope=2;
+		scopeCurator=2;
+		BoxloaderClass = "Land_Boxloader_Pallet_5";
+	};
 	class Land_Boxloader_Pallet_1: Boxloader_Pallet_Base {
 		model = "\boxloader\mdl\boxloader_pallet1.p3d";
 		displayName = "Boxloader Pallet Square";
@@ -167,6 +204,22 @@ class CfgVehicles {
 		};
 		class EventHandlers: EventHandlers {
 			init = "_this call boxloader_fnc_rack_init";
+		};
+	};
+	class Land_Boxloader_Pallet_5: Boxloader_Pallet_Base {
+		model = "\boxloader\mdl\boxloader_pallet5.p3d";
+		displayName = "463L Master Pallet";
+		class VehicleTransport: VehicleTransport {
+			class Cargo: Cargo {
+				dimensions[]                = {"BBox_Base0", "BBox_Corner"};
+			};
+			class Carrier: Carrier {
+				cargoBayDimensions[]        = {"BBox_Base1", "BBox_Corner"};
+				cargoAlignment[]            = {"left","front"};
+				cargoSpacing[]              = {0.05, 0.05, 0};
+				maxLoadMass                 = 45000;
+				disableHeightLimit          = 0;
+			};
 		};
 	};
 	class Land_Boxloader_iso20: Boxloader_Pallet_Base {
