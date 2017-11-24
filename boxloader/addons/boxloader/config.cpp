35336112ae967986ad349ead738fdb67a67e4416
vurtual <VurtualRuler98@gmail.com>
2017-11-23 21:08:20 -0600

    Tweaked ISO and TRICON to prevent clipping, added plastic hardcase WIP

--- boxloader/addons/boxloader/config.cpp
+++ boxloader/addons/boxloader/config.cpp
@@ -20,7 +20,10 @@ class CfgPatches {
 			"Land_Boxloader_iso_20ft_seabee",
 			"Land_Boxloader_iso_tricon_black",
 			"Land_Boxloader_iso_tricon_aid",
-			"Land_Boxloader_iso_tricon_seabee"
+			"Land_Boxloader_iso_tricon_seabee",
+			"Land_Boxloader_Case_Green",
+			"Land_Boxloader_Case_Aid",
+			"Land_Boxloader_Case_Tan"
 		};
 		weapons[] = {};
 		requiredVersion = 0.1;
@@ -91,6 +94,39 @@ class CfgVehicles {
 		ace_cargo_canLoad = 1;
 		ace_cargo_space = 4;
 		ace_Cargo_hasCargo = 1;
+	};
+	class Land_Boxloader_Case_Green: Reammobox_F {
+		ace_dragging_dragPosition[] = {0,1.5,0};
+		class VehicleTransport {
+			class Cargo {
+				parachuteClass              = "B_Parachute_02_F";
+				parachuteHeightLimit        = 5;
+				canBeTransported            = 1;
+			};
+		};
+		editorCategory="EdCat_Supplies";
+		editorSubcategory="EdSubCat_Boxloader";
+		displayName = "Military Case (Green)";
+		model = "\boxloader\mdl\boxloader_case.p3d";
+		icon = "iconObject_1x1";
+		scope=2;
+		scopeCurator=2;
+		side=3;
+		maximumLoad=5000;
+		ace_cargo_size = 4;
+		ace_cargo_canLoad = 1;
+		ace_cargo_space = 3;
+		ace_Cargo_hasCargo = 1;
+		hiddenSelections[] = {"case"};
+		hiddenSelectionsTextures[] = {"\boxloader\tex\boxloader_case_green_co.paa"};
+	};
+	class Land_Boxloader_Case_Aid: Land_Boxloader_Case_Green {
+		displayName = "Military Case (Red Crystal)";
+		hiddenSelectionsTextures[] = {"\boxloader\tex\boxloader_case_aid_co.paa"};
+	};
+	class Land_Boxloader_Case_Tan: Land_Boxloader_Case_Green {
+		displayName = "Military Case (Tan)";
+		hiddenSelectionsTextures[] = {"\boxloader\tex\boxloader_case_tan_co.paa"};
 	};
 	class Land_Boxloader_Crate_Roofrack: ThingX {
 		ace_dragging_dragPosition[] = {0,1.5,0};
