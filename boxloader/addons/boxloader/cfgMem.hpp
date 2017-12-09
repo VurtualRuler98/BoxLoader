4bc08c2ae65c747931dd8208da6be899887f3590
vurtual <VurtualRuler98@gmail.com>
2017-12-09 16:15:26 -0600

    WIP IFA glider stuff

--- boxloader/addons/boxloader/cfgMem.hpp
+++ boxloader/addons/boxloader/cfgMem.hpp
@@ -112,14 +112,18 @@ class Land_Boxloader_mem_ifa: Land_Boxloader_membase_truck {
 };
 class Land_Boxloader_mem_ifazis: Land_Boxloader_membase_truck {
 	model = "\boxloader\mdl\boxloader_mem_ifazis.p3d";
-	class VehicleTransport: VehicleTransport {
-		class Carrier: Carrier {
-			exits[]                     = {"BBox_Exit"};
-		};
-	};
+
 };
 class Land_Boxloader_mem_ifac47: Land_Boxloader_membase_truck {
 	model = "\boxloader\mdl\boxloader_mem_ifac47.p3d";
+	class VehicleTransport: VehicleTransport {
+		class Carrier: Carrier {
+			disableHeightLimit = 0;
+		};
+	};
+};
+class Land_Boxloader_mem_ifawaco_rear: Land_Boxloader_membase_truck {
+	model = "\boxloader\mdl\boxloader_mem_ifawaco_rear.p3d";
 	class VehicleTransport: VehicleTransport {
 		class Carrier: Carrier {
 			disableHeightLimit = 0;
